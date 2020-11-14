ea7a5c24597192ea591aef9af3d90eaebc56caa7
PYF <pengyunfei@ggn.top>
2020-11-14 18:30:15 +0800

    feat(global): :tada: fisrt commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <Provider store={store}>
+    <React.Suspense fallback={<h1>loading</h1>}>
+      <App />
+    </React.Suspense>
+  // </Provider>
+  ,
+  
   document.getElementById('root')
 );
 
---
+++ src/views/Layout/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { Layout, Menu } from 'antd'
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
+interface IProps {
+  children ?: Element
+}
+
+const LayOut = (props: IProps) => {
+    const [show, setShow] = React.useState(false);
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={show}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(show ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: () => setShow(show => !show),
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    )
+}
+
+export default LayOut;
